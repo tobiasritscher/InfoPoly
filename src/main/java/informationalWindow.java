7691941edaa78e1160420fca7b7a10f5237d4518
Oliver <corrooli@students.zhaw.ch>
2020-04-29 22:24:06 +0200

    code(Closes #8): Create informational window UI
    Implemented a simple, lightweight and elegant way to display information. It's easy to create, just type --> new informationalWindow("and the text you want to spout at the user");

---
+++ src/main/java/informationalWindow.java
@@ -0,0 +1,34 @@
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
+
+/**
+ * Informational window to inform players of events happening in the game.
+ * (e.g. chance field, party field, zvv fee field)
+ * To create an informational window, you don't need to instance it permanently. You can simply create one by using:
+ * new informationalWindow("The text you want to tell the user);
+ */
+public class informationalWindow {
+
+    /**
+     * Spawns a new alert window.
+     */
+    private Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    /**
+     * Header and title text
+     */
+    private final String header = "Information";
+
+    /**
+     * Constructor of the informational window.
+     *
+     * @param message The message for the player.
+     */
+    public informationalWindow(String message) {
+        alert.setTitle(header);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+        Optional<ButtonType> buttonType = alert.showAndWait();
+    }
+}
