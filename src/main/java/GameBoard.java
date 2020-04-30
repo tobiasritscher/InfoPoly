0721f89e99445cd77f8c66a0564bc6423a5b51a2
Nikita Smailov <smailnik@students.zhaw.ch>
2020-04-30 22:42:29 +0200

    implement (#14): GameBoard
    > add class fields:
      > board: ArrayList<GameField>
    > add methods:
      > getField( int fieldId) : GameField
    > add inner class GameField

---
+++ src/main/java/GameBoard.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * The class stores game fields of the game board.
+ *
+ * Allows to access a field using it's id.
+ *
+ */
+public class GameBoard {
+
+    private ArrayList<GameField> board;
+
+    GameField getField(int fieldId) {
+        //todo implement
+        return null;
+    }
+
+    public class GameField {
+        //todo implement
+    }
+}
