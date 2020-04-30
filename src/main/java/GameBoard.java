e825cd2b34a8556a6bb639dcd8e54cc462f24f4c
Nikita Smailov <smailnik@students.zhaw.ch>
2020-04-30 22:47:59 +0200

    implement (#14): GameBoard.getField()

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -2,17 +2,21 @@ import java.util.ArrayList;
 
 /**
  * The class stores game fields of the game board.
- *
+ * <p>
  * Allows to access a field using it's id.
- *
  */
 public class GameBoard {
 
     private ArrayList<GameField> board;
 
     GameField getField(int fieldId) {
-        //todo implement
-        return null;
+        if (fieldId < 0) {
+            throw new RuntimeException("invalid index: must be positive");
+        }
+        if (fieldId >= board.size()) {
+            throw new RuntimeException("invalid index: out of bound");
+        }
+        return board.get(fieldId);
     }
 
     public class GameField {
