274e95fe0b3664b1048a072bb00cf60a66ef1e9e
Oliver <corrooli@students.zhaw.ch>
2020-05-05 09:40:26 +0200

    Code (Closes #35) Refactor GameController into InfoPoly
    Since MainWindowController instances Logic and GameBoard objects, GameController has been rendered obsolete. It needs to be renamed to make more sense with the MVC / MVP pattern. This has been discussed with @muon.

---
+++ src/main/java/InfoPoly.java
@@ -0,0 +1,30 @@
+/**
+ *  /$$$$$$            /$$$$$$          /$$$$$$$           /$$
+ * |_  $$_/           /$$__  $$        | $$__  $$         | $$
+ *   | $$   /$$$$$$$ | $$  \__//$$$$$$ | $$  \ $$ /$$$$$$ | $$ /$$   /$$
+ *   | $$  | $$__  $$| $$$$   /$$__  $$| $$$$$$$//$$__  $$| $$| $$  | $$
+ *   | $$  | $$  \ $$| $$_/  | $$  \ $$| $$____/| $$  \ $$| $$| $$  | $$
+ *   | $$  | $$  | $$| $$    | $$  | $$| $$     | $$  | $$| $$| $$  | $$
+ *  /$$$$$$| $$  | $$| $$    |  $$$$$$/| $$     |  $$$$$$/| $$|  $$$$$$$
+ * |______/|__/  |__/|__/     \______/ |__/      \______/ |__/ \____  $$
+ *                                                             /$$  | $$
+ *                                                            |  $$$$$$/
+ *                                                             \______/
+ *
+ * Entry point and host of main method of the InfoPoly game. Instances a new View, and with it, starts the entire game.
+ *
+ * @author ritsctob, zankoerm, smailnik, corrooli
+ */
+public class InfoPoly {
+
+    public static void main(String[] args) {
+        setUpGame();
+    }
+    /**
+     * Starts an UI (and a new game)
+     */
+    private static void setUpGame() {
+        MainWindowView view = new MainWindowView();
+        view.startUI();
+    }
+}
