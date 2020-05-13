5a160330d318df1c2d9cbe6cb9c40390915f67f5
Nikita Smailov <smailnik@students.zhaw.ch>
2020-05-13 19:13:24 +0200

    code(#62): add test cases, fix bugs
    game fields:
    > Exam
    > Job
    > Repetition
    > Start
    > Startup

--- src/test/java/ch/zhaw/pm2/caffeineaddicts/infopoly/model/GameBoardTest.java
+++ src/test/java/ch/zhaw/pm2/caffeineaddicts/infopoly/model/GameBoardTest.java
@@ -27,22 +27,12 @@ class GameBoardTest {
     }
 
     @Test
-    void loadGameBoard() {
-        fields.add(new ModuleGameField(0, "module"));
-        fields.add(new ModuleGameField(1, "module"));
-        fields.add(new ModuleGameField(2, "module"));
-        Config.setFieldLayoutPath("src/test/resources/test-gameboard-layout.txt");
-        gameBoard = new GameBoard();
-        assertEquals(fields, gameBoard.board);
-    }
-
-    @Test
     void loadGameBoardDifferentFields() {
         fields.add(new FeeGameField(0, "fee", FeeGameField.FeeType.RANDOM));
         fields.add(new ModuleGameField(1, "module"));
         fields.add(new StartupGameField(2, "startup"));
         fields.add(new RepetitionGameField(3, "repetition"));
-        Config.setFieldLayoutPath("src/test/resources/test-gameboard-different-fields.txt");
+        Config.setFieldLayoutPath("src/test/resources/GameBoardTest/test-gameboard-different-fields.txt");
         gameBoard = new GameBoard();
         Assertions.assertTrue(gameBoard.getBoard().get(0) instanceof FeeGameField);
         Assertions.assertTrue(gameBoard.getBoard().get(1) instanceof ModuleGameField);
@@ -51,50 +41,133 @@ class GameBoardTest {
         assertEquals(fields, gameBoard.board);
     }
 
-    @Test
-    void loadGameBoardModuleGameField() {
-        final String moduleName = "test module";
-        final int fieldId = 0;
-        fields.add(new ModuleGameField(fieldId, moduleName));
-        Config.setFieldLayoutPath("src/test/resources/test-gameboard-module.txt");
-        gameBoard = new GameBoard();
-        Assertions.assertTrue(gameBoard.getBoard().get(fieldId) instanceof ModuleGameField);
-        Assertions.assertEquals(fieldId, gameBoard.getBoard().get(fieldId).getFieldId());
-        Assertions.assertEquals(fields.get(fieldId).getFieldName(), moduleName);
-    }
-
+    /**
+     * Checks if {@link ChanceGameField} is correctly initialized from the file.
+     */
     @Test
     void loadGameBoardChanceGameField() {
         final String moduleName = "test chance";
         final int fieldId = 0;
         fields.add(new ChanceGameField(fieldId, moduleName));
-        Config.setFieldLayoutPath("src/test/resources/test-gameboard-chance.txt");
+        Config.setFieldLayoutPath("src/test/resources/GameBoardTest/test-gameboard-chance.txt");
         gameBoard = new GameBoard();
         Assertions.assertTrue(gameBoard.getBoard().get(fieldId) instanceof ChanceGameField);
         Assertions.assertEquals(fieldId, gameBoard.getBoard().get(fieldId).getFieldId());
         Assertions.assertEquals(moduleName, gameBoard.getBoard().get(fieldId).getFieldName());
     }
 
+    /**
+     * Checks if {@link ExamGameField} is correctly initialized from the file.
+     */
+    @Test
+    void loadGameBoardExamGameField() {
+        final String name = "test exam";
+        final int fieldId = 0;
+        fields.add(new ExamGameField(fieldId, name));
+        Config.setFieldLayoutPath("src/test/resources/GameBoardTest/test-gameboard-exam.txt");
+        gameBoard = new GameBoard();
+        Assertions.assertTrue(gameBoard.getBoard().get(fieldId) instanceof ExamGameField);
+        Assertions.assertEquals(fieldId, gameBoard.getBoard().get(fieldId).getFieldId());
+        Assertions.assertEquals(name, gameBoard.getBoard().get(fieldId).getFieldName());
+    }
+
+    /**
+     * Checks if {@link FeeGameField} is correctly initialized from the file.
+     */
     @Test
     void loadGameBoardFeeGameField() {
-        final String moduleName = "test fee";
+        final String name = "test fee";
         final int fieldIdOne = 0;
         final int fieldIdTwo = 1;
-        fields.add(new FeeGameField(fieldIdOne, moduleName, FeeGameField.FeeType.RANDOM));
-        fields.add(new FeeGameField(fieldIdOne, moduleName, FeeGameField.FeeType.FIXED));
+        fields.add(new FeeGameField(fieldIdOne, name, FeeGameField.FeeType.RANDOM));
+        fields.add(new FeeGameField(fieldIdOne, name, FeeGameField.FeeType.FIXED));
 
-        Config.setFieldLayoutPath("src/test/resources/test-gameboard-fee.txt");
+        Config.setFieldLayoutPath("src/test/resources/GameBoardTest/test-gameboard-fee.txt");
         gameBoard = new GameBoard();
 
         Assertions.assertTrue(gameBoard.getBoard().get(fieldIdOne) instanceof FeeGameField);
         Assertions.assertEquals(fieldIdOne, gameBoard.getBoard().get(fieldIdOne).getFieldId());
-        Assertions.assertEquals(moduleName, gameBoard.getBoard().get(fieldIdOne).getFieldName());
+        Assertions.assertEquals(name, gameBoard.getBoard().get(fieldIdOne).getFieldName());
         Assertions.assertEquals(FeeGameField.FeeType.RANDOM, ((FeeGameField) gameBoard.getBoard().get(fieldIdOne)).getFeeType());
 
         Assertions.assertTrue(gameBoard.getBoard().get(fieldIdTwo) instanceof FeeGameField);
         Assertions.assertEquals(fieldIdTwo, gameBoard.getBoard().get(fieldIdTwo).getFieldId());
-        Assertions.assertEquals(moduleName, gameBoard.getBoard().get(fieldIdTwo).getFieldName());
+        Assertions.assertEquals(name, gameBoard.getBoard().get(fieldIdTwo).getFieldName());
         Assertions.assertEquals(FeeGameField.FeeType.FIXED, ((FeeGameField) gameBoard.getBoard().get(fieldIdTwo)).getFeeType());
 
+    }
+
+    /**
+     * Checks if {@link JobGameField} is correctly initialized from the file.
+     */
+    @Test
+    void loadGameBoardJobGameField() {
+        final String name = "test job";
+        final int fieldId = 0;
+        fields.add(new JobGameField(fieldId, name));
+        Config.setFieldLayoutPath("src/test/resources/GameBoardTest/test-gameboard-job.txt");
+        gameBoard = new GameBoard();
+        Assertions.assertTrue(gameBoard.getBoard().get(fieldId) instanceof JobGameField);
+        Assertions.assertEquals(fieldId, gameBoard.getBoard().get(fieldId).getFieldId());
+        Assertions.assertEquals(name, gameBoard.getBoard().get(fieldId).getFieldName());
+    }
+
+    /**
+     * Checks if {@link ModuleGameField} is correctly initialized from the file.
+     */
+    @Test
+    void loadGameBoardModuleGameField() {
+        final String name = "test module";
+        final int fieldId = 0;
+        fields.add(new ModuleGameField(fieldId, name));
+        Config.setFieldLayoutPath("src/test/resources/GameBoardTest/test-gameboard-module.txt");
+        gameBoard = new GameBoard();
+        Assertions.assertTrue(gameBoard.getBoard().get(fieldId) instanceof ModuleGameField);
+        Assertions.assertEquals(name, gameBoard.getBoard().get(fieldId).getFieldName());
+    }
+
+    /**
+     * Checks if {@link RepetitionGameField} is correctly initialized from the file.
+     */
+    @Test
+    void loadGameBoardRepetitionGameField() {
+        final String name = "test repetition";
+        final int fieldId = 0;
+        fields.add(new RepetitionGameField(fieldId, name));
+        Config.setFieldLayoutPath("src/test/resources/GameBoardTest/test-gameboard-repetition.txt");
+        gameBoard = new GameBoard();
+        Assertions.assertTrue(gameBoard.getBoard().get(fieldId) instanceof RepetitionGameField);
+        Assertions.assertEquals(fieldId, gameBoard.getBoard().get(fieldId).getFieldId());
+        Assertions.assertEquals(name, gameBoard.getBoard().get(fieldId).getFieldName());
+    }
+
+    /**
+     * Checks if {@link StartGameField} is correctly initialized from the file.
+     */
+    @Test
+    void loadGameBoardStartGameField() {
+        final String name = "test start";
+        final int fieldId = 0;
+        fields.add(new StartGameField(fieldId, name));
+        Config.setFieldLayoutPath("src/test/resources/GameBoardTest/test-gameboard-start.txt");
+        gameBoard = new GameBoard();
+        Assertions.assertTrue(gameBoard.getBoard().get(fieldId) instanceof StartGameField);
+        Assertions.assertEquals(fieldId, gameBoard.getBoard().get(fieldId).getFieldId());
+        Assertions.assertEquals(fields.get(fieldId).getFieldName(), name);
+    }
+
+    /**
+     * Checks if {@link StartupGameField} is correctly initialized from the file.
+     */
+    @Test
+    void loadGameBoardStartupGameField() {
+        final String name = "test startup";
+        final int fieldId = 0;
+        fields.add(new StartupGameField(fieldId, name));
+        Config.setFieldLayoutPath("src/test/resources/GameBoardTest/test-gameboard-startup.txt");
+        gameBoard = new GameBoard();
+        Assertions.assertTrue(gameBoard.getBoard().get(fieldId) instanceof StartupGameField);
+        Assertions.assertEquals(fieldId, gameBoard.getBoard().get(fieldId).getFieldId());
+        Assertions.assertEquals(fields.get(fieldId).getFieldName(), name);
     }
 }
